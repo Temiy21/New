f31dadf923a31f2492dc67fa5ac79c1ea92c98a4
Temiy21 <98648320+temiy21@users.noreply.github.com>
2022-12-16 16:36:35 +0300

    Initial commit

---
+++ 1.py
@@ -0,0 +1,13 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QPushButton, QWidget
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(300, 500)
+        self.setWindowTitle("Test")
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
