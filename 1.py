40d79a5112a3077d09f1469641f9cbffd97bf863
artem <aart7116@gmail.com>
2022-12-16 16:41:59 +0300

    resize

--- 1.py
+++ 1.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QApplication, QPushButton, QWidget
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(300, 500)
+        self.resize(300, 200)
         self.setWindowTitle("Test")
 app = QApplication(sys.argv)
 window = MainWindow()
